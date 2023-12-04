18cd25f84dda88e6340da12e35e830e4d9afcfc3
Michele Noten <notenm@gmail.com>
2023-12-04 13:08:35 +0800

    making product page dynamic, weighted average score defined

--- app/controllers/wardrobes_controller.rb
+++ app/controllers/wardrobes_controller.rb
@@ -6,4 +6,14 @@ class WardrobesController < ApplicationController
   def index_products
     @products = Product.all
   end
+
+  def show
+    @wardrobe = Wardrobe.find(params[:id])
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    current_user.wardrobe.products << @product
+    redirect_to wardrobes_path, notice: 'Product added to wardrobe successfully.'
+  end
 end
