9ec84831c62b2b7d7643e533aec48ca3047ab626
Ankit Kumar <ankit@gypsyphi.com>
2023-12-04 19:26:55 +0800

    Model depenndencies

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :scan, optional: true
 
-  has_many :product_fabrics
-  has_many_attached :photos
+  has_many :product_fabrics, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
+  has_many :wardrobes, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 end
