0970778cf95f00223f7b60d2d7e60c431bc56241
Michele Noten <notenm@gmail.com>
2023-12-04 17:00:45 +0800

    dynamic form, javascript for dynamic form, and create

---
+++ app/javascript/controllers/fabric_list_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="fabric-list"
+export default class extends Controller {
+  static targets = ["fabric", "list", "counter"];
+
+  add() {
+    const counter = this.fabricTargets.length + 1;
+    console.log(counter);
+    const input = `<div class="fabric-container" data-fabric-list-target="fabric">
+                    <div class="fabric mb-3">
+                    <label for="fabric_type_${counter}" class="form-label">Fabric type</label>
+                    <input type="text" class="form-control" name="fabric_type_${counter}" id="fabric_type_${counter}">
+                  </div>
+                  <div class="fabric">
+                    <label for="fabric_composition_${counter}" class="form-label">Percentage (%)</label>
+                    <input type="text" name="fabric_composition_${counter}" class="form-control" id="fabric_composition_${counter}">
+                  </div>
+                  </div>`;
+    this.listTarget.insertAdjacentHTML("beforeend", input);
+    this.counterTarget.value = counter;
+  }
+}
