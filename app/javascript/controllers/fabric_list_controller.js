00233a03811adc3cd82f51ca5ddc4a76f8d15b0f
Michele Noten <notenm@gmail.com>
2023-12-05 11:40:08 +0800

    deleting & adding items from the edit form

--- app/javascript/controllers/fabric_list_controller.js
+++ app/javascript/controllers/fabric_list_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="fabric-list"
 export default class extends Controller {
-  static targets = ["fabric", "list", "counter"];
+  static targets = ["fabric", "list", "counter", "deletedItems"];
 
   connect() {
     console.log("Hello from fabric list controller");
@@ -20,21 +20,14 @@ export default class extends Controller {
                     <label for="fabric_composition_${counter}" class="form-label">Percentage (%)</label>
                     <input type="text" name="fabric_composition_${counter}" class="form-control" id="fabric_composition_${counter}">
                   </div>
-                  <button type="button" data-action="click->fabric-list#removeFabric" style="background: none; border: none; color: black; cursor: pointer;">X</button>
+                  <button type="button" data-id=${counter} data-action="click->fabric-list#removeFabric" style="background: none; border: none; color: black; cursor: pointer;">X</button>
                   </div>`;
     this.listTarget.insertAdjacentHTML("beforeend", input);
     this.counterTarget.value = counter;
   }
 
   removeFabric(event) {
-    const fabricContainers = this.element.querySelectorAll('[data-fabric-list-target="fabric"]');
-    const lastFabricContainer = fabricContainers[fabricContainers.length - 1];
-
-    if (lastFabricContainer) {
-      lastFabricContainer.remove();
-
-      const counter = this.fabricTargets.length;
-      this.counterTarget.value = counter;
-    }
+    this.deletedItemsTarget.value += event.currentTarget.dataset.type + ",";
+    event.currentTarget.parentElement.remove();
   }
 }
