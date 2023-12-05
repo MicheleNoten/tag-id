13fe86b2c58df7358d7b27bae7a2a956a795af03
Kimberly Agustin van der Leeuw <kimberlyavdl@gmail.com>
2023-12-05 12:11:32 +0800

    Search bar working for fabrics as well

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,15 +4,17 @@ class Product < ApplicationRecord
   belongs_to :scan, optional: true
 
   has_many :product_fabrics
+  has_many :fabrics, through: :product_fabrics
   has_many_attached :photos
 
   include PgSearch::Model
   pg_search_scope :global_search,
-    against: [ :brand, :item_name ],
+    against: [ :brand, :item_name, :made_in, :purchased_in ],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     },
     associated_against: {
-      category: [ :name ]
+      category: [:name],
+      fabrics: [:name]
     }
 end
